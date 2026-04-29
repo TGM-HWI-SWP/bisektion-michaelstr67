0e8c42997d3c71acc50904ec572a3a2f29814ecb
michaelstr67 <mstrohmayer@student.tgm.ac.at>
2026-04-29 20:06:11 +0200

    Aufgabe 5&6 erledigt

--- Aufgabe5.py
+++ Aufgabe5.py
@@ -1,8 +1,72 @@
+import math
+
+
+def funktion_aus_text_erstellen(formel: str):
+    erlaubte_werte = {
+        "x": 0,
+        "math": math,
+        "sin": math.sin,
+        "cos": math.cos,
+        "tan": math.tan,
+        "sqrt": math.sqrt,
+        "sinh": math.sinh,
+        "cosh": math.cosh,
+        "pi": math.pi,
+        "e": math.e,
+    }
+
+    def f(x: float) -> float:
+        erlaubte_werte["x"] = x
+        return eval(formel, {"__builtins__": {}}, erlaubte_werte)
+
+    return f
+
+
+def bisektion(f,a ,b , epsilon):
+    try:
+        if f(a) * f(b) >= 0:
+            print("Kein Vorzeichenwechsel")
+            return None
+
+        while (b - a) / 2 > epsilon:
+            c = (a + b) / 2
+
+            if f(a) * f(c) < 0:
+                b = c
+            else:
+                a = c
+
+        return (a + b) / 2
+
+    except:
+        print("Fehler")
+        return None
 
 
 def solver():
-    pass
+   print("=== Aufgabe 5 ===")
+   print("1 - Eigene Funktion")
+   print("2 - Testfälle")
 
+   wahl = input("Auswahl: ")
+
+   if wahl == "1":
+    formel = input("Funktion: ")
+    a = float(input("a: "))
+    b = float(input("b: "))
+    epsilon = float(input("epsilon: "))
+
+    f = funktion_aus_text_erstellen(formel)
+    x = bisektion(f, a, b, epsilon)
+
+    print("Nullstelle:", x)
+
+   elif wahl == "2":
+    for n in [25, 81, 144]:
+        f = funktion_aus_text_erstellen(f"x**2 - {n}")
+        x = bisektion(f, 0, n, 0.00001)
+
+        print("n =", n, "| numerisch:", x, "| exakt:", math.sqrt(n)) 
 
 if __name__ == "__main__":
     
--- Aufgabe6.py
+++ Aufgabe6.py
@@ -1,7 +1,47 @@
+import math
+from Aufgabe5 import funktion_aus_text_erstellen
+
+
+def regula_falsi(f, a, b, epsilon):
+    while True:
+        c = b - f(b) * (b - a) / (f(b) - f(a))
+
+        if abs(f(c)) < epsilon:
+            return c
+
+        if f(a) * f(c) < 0:
+            a = c
+        else:
+            b = c
+
+
+
 
 
 def solver2():
-    pass
+    print("=== Aufgabe 6 ===")
+    print("1 - Eigene Funktion")
+    print("2 - Testfälle")
+
+    wahl = input("Auswahl: ")
+
+    if wahl == "1":
+        formel = input("Funktion: ")
+        a = float(input("a: "))
+        b = float(input("b: "))
+        epsilon = float(input("epsilon: "))
+
+        f = funktion_aus_text_erstellen(formel)
+        x = regula_falsi(f, a, b, epsilon)
+
+        print("Nullstelle:", x)
+
+    elif wahl == "2":
+        for n in [25, 81, 144]:
+            f = funktion_aus_text_erstellen(f"x**2 - {n}")
+            x = regula_falsi(f, 0, n, 0.00001)
+
+            print("n =", n, "| numerisch:", x, "| exakt:", math.sqrt(n))
 
 
 if __name__ == "__main__":
